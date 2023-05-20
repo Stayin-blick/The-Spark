362b805f52aa39f66067e9c482df933f4d696290
Stayin-blick <>
2023-05-20 12:49:27 +0000

    fixes

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,6 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 
 class Post(models.Model):
-
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
@@ -31,7 +30,6 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, related_name='comments')
     name = models.CharField(max_length=80)
