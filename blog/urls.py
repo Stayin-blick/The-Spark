ae6d007e54db13377ff01c158e6ba2fc56ede6a1
Stayin-blick <>
2023-05-24 10:23:19 +0000

    create post url added

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_details'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
+    path('create_post/', views.Create_Post.as_view(), name='create_post'),
 ]
