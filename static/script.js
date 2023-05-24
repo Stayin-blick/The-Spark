6090551188e2e852c6861f745102bc34c3f47203
Stayin-blick <>
2023-05-24 10:22:59 +0000

    fromating

--- static/script.js
+++ static/script.js
@@ -1,35 +1,37 @@
 // Get the buttons
-const createPostBtn = document.querySelector('.btn-primary:nth-of-type(1)');
-const topLikesBtn = document.querySelector('.btn-primary:nth-of-type(2)');
-const topCommentsBtn = document.querySelector('.btn-primary:nth-of-type(3)');
+const createPostBtn = document.querySelector(".btn-primary:nth-of-type(1)");
+const topLikesBtn = document.querySelector(".btn-primary:nth-of-type(2)");
+const topCommentsBtn = document.querySelector(".btn-primary:nth-of-type(3)");
 
 // Get the blog post container
-const blogPostContainer = document.querySelector('.container-fluid.rounded-pill.text-center');
+const blogPostContainer = document.querySelector(
+  ".container-fluid.rounded-pill.text-center"
+);
 
 // Add event listeners to the buttons
-createPostBtn.addEventListener('mouseenter', darkenButton);
-createPostBtn.addEventListener('mouseleave', restoreButton);
-createPostBtn.addEventListener('click', redirectToCreatePost);
+createPostBtn.addEventListener("mouseenter", darkenButton);
+createPostBtn.addEventListener("mouseleave", restoreButton);
+createPostBtn.addEventListener("click", redirectToCreatePost);
 
-topLikesBtn.addEventListener('mouseenter', darkenButton);
-topLikesBtn.addEventListener('mouseleave', restoreButton);
-topLikesBtn.addEventListener('click', sortPostsByLikes);
+topLikesBtn.addEventListener("mouseenter", darkenButton);
+topLikesBtn.addEventListener("mouseleave", restoreButton);
+topLikesBtn.addEventListener("click", sortPostsByLikes);
 
-topCommentsBtn.addEventListener('mouseenter', darkenButton);
-topCommentsBtn.addEventListener('mouseleave', restoreButton);
-topCommentsBtn.addEventListener('click', sortPostsByComments);
+topCommentsBtn.addEventListener("mouseenter", darkenButton);
+topCommentsBtn.addEventListener("mouseleave", restoreButton);
+topCommentsBtn.addEventListener("click", sortPostsByComments);
 
 // Function to darken the button
 function darkenButton(event) {
-    event.target.classList.add('darken');
+  event.target.classList.add("darken");
 }
 
 // Function to restore the button to its original state
 function restoreButton(event) {
-    event.target.classList.remove('darken');
+  event.target.classList.remove("darken");
 }
 
 // Function to redirect to the create_post.html page
 function redirectToCreatePost() {
-    window.location.href = 'create_post.html';
+  window.location.href = "create_post.html";
 }
