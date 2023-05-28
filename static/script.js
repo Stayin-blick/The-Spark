0550e0fa399286961f6802f0e0ebae3fae0c2079
Stayin-blick <>
2023-05-28 10:48:03 +0000

    added reset functionailty to sorting buttons

--- static/script.js
+++ static/script.js
@@ -1,38 +1,55 @@
 // interactive bar
-//sorting script (Top Likes/Top Comments)
+//sorting script (Top Likes/Top Comments/Most Recent)
 
 const sortLikesBtn = document.getElementById('sortLikesBtn');
 const sortCommentsBtn = document.getElementById('sortCommentsBtn');
 
-//assigned event listener
+// Variables to track the current sorting state
+let likesSortAscending = false;
+let commentsSortAscending = false;
+
+//assign event listener
 sortLikesBtn.addEventListener('click', sortPostsByLikes);
 sortCommentsBtn.addEventListener('click', sortPostsByComments);
+mostRecentBtn.addEventListener('click', sortPostsByRecent);
 
+// sort by likes function
 function sortPostsByLikes() {
     const blogPosts = Array.from(document.querySelectorAll('.blog-post'));
     blogPosts.sort((a, b) => {
         const likesA = parseInt(a.dataset.likes);
         const likesB = parseInt(b.dataset.likes);
-        return likesB - likesA;
+        if (likesSortAscending) {
+            return likesA - likesB;
+        } else {
+            return likesB - likesA;
+        }
     });
+    likesSortAscending = !likesSortAscending;
     rearrangePosts(blogPosts);
 }
 
+// sort by comments function
 function sortPostsByComments() {
     const blogPosts = Array.from(document.querySelectorAll('.blog-post'));
     blogPosts.sort((a, b) => {
         const commentsA = parseInt(a.dataset.comments);
         const commentsB = parseInt(b.dataset.comments);
-        return commentsB - commentsA;
+        if (commentsSortAscending) {
+            return commentsA - commentsB;
+        } else {
+            return commentsB - commentsA;
+        }
     });
+    commentsSortAscending = !commentsSortAscending;
     rearrangePosts(blogPosts);
-}
 
 function rearrangePosts(sortedPosts) {
     const container = document.getElementById('blog-post');
     sortedPosts.forEach(post => container.appendChild(post));
 }
 
+//tag search bar
 document.addEventListener('DOMContentLoaded', function () {
     const form = document.getElementById('search-form');
     const input = document.getElementById('tag-input');
@@ -69,41 +86,3 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
-
-document.addEventListener('DOMContentLoaded', function () {
-    const form = document.getElementById('search-form');
-    const input = document.getElementById('tag-input');
-    const blogPosts = document.getElementsByClassName('blog-post');
-
-    form.addEventListener('submit', function (event) {
-        event.preventDefault();
-        const searchValue = input.value.toLowerCase();
-
-        for (let i = 0; i < blogPosts.length; i++) {
-            const post = blogPosts[i];
-            const tags = post.dataset.tags.toLowerCase();
-
-            if (searchValue === '' || tags.includes(searchValue)) {
-                post.style.display = 'block';
-            } else {
-                post.style.display = 'none';
-            }
-        }
-    });
-
-    input.addEventListener('input', function () {
-        const searchValue = input.value.toLowerCase();
-
-        for (let i = 0; i < blogPosts.length; i++) {
-            const post = blogPosts[i];
-            const tags = post.dataset.tags.toLowerCase();
-
-            if (searchValue === '' || tags.includes(searchValue)) {
-                post.style.display = 'block';
-            } else {
-                post.style.display = 'none';
-            }
-        }
-    });
-});
-
