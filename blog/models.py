10a6a5c0075d3c6aa577081de3edba4a5bf67482
Stayin_blick <azeezbello75@gmail.com>
2023-09-27 14:10:48 +0000

    adapted model for userprofile

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    profile_picture = CloudinaryField('image', default='/blog/images/default_user.png')
     bio = models.TextField(blank=True)
 
     def __str__(self):
--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('create_post/', views.Create_Post.as_view(), name='create_post'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_details'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
+    path('user_profile/', views.UserProfile.as_view(), name='user_profile'),
 
 ]
