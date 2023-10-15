194ccd797334b47a48a607a84822aa680bc1f40d
Stayin_blick <azeezbello75@gmail.com>
2023-10-15 17:06:41 +0000

    custom sign up page

--- The_Spark/settings.py
+++ The_Spark/settings.py
@@ -174,3 +174,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# 
+ACCOUNT_FORMS = {
+    'signup': 'blog.forms.CustomSignupForm',
+}
--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,8 @@
 from .models import Comment, Post, UserProfile
 from django import forms
 from django.contrib.auth.forms import UserChangeForm
-
+from allauth.account.forms import SignupForm
+from django.contrib.auth.models import User
 
 class UserProfileForm(UserChangeForm):
     class Meta:
@@ -25,3 +26,37 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+
+class CustomSignupForm(SignupForm):
+
+    username = forms.CharField(
+        label="Username",
+        max_length=30,
+        required=True,
+        widget=forms.TextInput(attrs={'placeholder': 'Enter a unique username'}),
+    )
+
+    #  password fields with verification requirements
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput,
+    )
+    password2 = forms.CharField(
+        label="Password confirmation",
+        widget=forms.PasswordInput,
+    )
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError("This username is already taken.")
+        return username
+
+    def clean(self):
+        cleaned_data = super(CustomSignupForm, self).clean()
+        password1 = cleaned_data.get('password1')
+        password2 = cleaned_data.get('password2')
+
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords do not match.")
+        return cleaned_data
